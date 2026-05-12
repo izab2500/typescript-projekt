335ac5791d59b012c3c9cc3e8dd6bd74b50236c6
ziba <ziba2500@student.miun.se>
2026-05-12 23:18:51 +0200

    Configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Course } from './pages/course/course';
+import { StudyPlan } from './pages/study-plan/study-plan';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home,
+        title: "Öppna akademin | Hem"
+    },
+    {
+        path: "kurser",
+        component: Course,
+        title: "Öppna akademin | Kurser"
+
+    },
+    {
+        path: "ramschema",
+        component: StudyPlan,
+        title: "Öppna akademin | ramschema"
+    },
+    {
+        path: "om",
+        component: About,
+        title: "Öppna akademin | Om"
+
+    },
+    {
+        path: "kontakt",
+        component: Contact,
+        title: "Öppna akademin | Kontakt"
+
+    },
+
+    {
+        path: '404',
+        component: NotFound,
+        title: "Öppna akademin | 404"
+    },
+    {
+        path: '**',
+        redirectTo: '404'
+    }
+
+];
